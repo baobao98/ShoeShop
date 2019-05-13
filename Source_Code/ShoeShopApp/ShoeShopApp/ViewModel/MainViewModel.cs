22cb383df96254b7b034c5a9ce994d652f9b8e1b
baobao98 <lamphuocbao98@gmail.com>
2019-05-13 16:07:19 +0700

    initial project

---
+++ Source_Code/ShoeShopApp/ShoeShopApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeShopApp.ViewModel
+{
+    public class MainViewModel: BaseViewModel
+    {
+        private string _textContent;
+        public string textContent { get => _textContent; set { _textContent = value; OnPropertyChanged(); } }
+
+        public MainViewModel()
+        {
+            textContent = "Hello World";
+        }
+
+    }
+}
