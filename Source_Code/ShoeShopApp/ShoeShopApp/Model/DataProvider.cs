33643df23123dae8c340515cb084f21616e4b57d
baobao98 <lamphuocbao98@gmail.com>
2019-05-15 21:51:09 +0700

    connect database to azure.

---
+++ Source_Code/ShoeShopApp/ShoeShopApp/Model/DataProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoeShopApp.Model;
+
+namespace ShoeShopApp.Model
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+
+        public ShoeShopEntities db { get; set; }
+        private DataProvider()
+        {
+            db = new ShoeShopEntities();
+        }
+    }
+}
--- Source_Code/ShoeShopApp/ShoeShopApp/ViewModel/MainViewModel.cs
+++ Source_Code/ShoeShopApp/ShoeShopApp/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShoeShopApp.Model;
 
 namespace ShoeShopApp.ViewModel
 {
@@ -13,7 +14,10 @@ namespace ShoeShopApp.ViewModel
 
         public MainViewModel()
         {
-            textContent = "Hello World";
+            List<NhaCungCap> nhaCungCaps = new List<NhaCungCap>();
+            nhaCungCaps = DataProvider.Ins.db.NhaCungCaps.ToList();
+            textContent ="Hello World!!! This text is loaded from database (table NhaCungCap): <"+nhaCungCaps[0].TenNCC+"> " +
+                "Test connect successfully";
         }
 
     }
